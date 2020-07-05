00cbc84b1ca0d1206d78bb8f2d233621b5335083
netpipe <teslaenergy@gmail.com>
2020-07-05 11:43:39 -0600

    qpipe

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "mainwindow.h"
+#include "qonsole.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    Qonsole w;
     w.show();
 
     return a.exec();
